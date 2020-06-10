d1d817e544f91d529470be8b296ea2edd8385884
Amel Islamovic <aislqmovic@gmail.com>
2020-06-10 18:09:57 +0200

    Added functionality if screen width is under 500px display none

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SortingVisualizer from "./components/SortingVisualizer/SortingVisualizer"
 function App() {
   return (
     <div className="App">
+      <h1 className="happ">Screen Width is too small!!</h1>
       <SortingVisualizer />
     </div>
   );
