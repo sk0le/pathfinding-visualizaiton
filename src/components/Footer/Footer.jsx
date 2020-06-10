cae384ee4f2b1282cbb727f4231054dda27fa197
Amel Islamovic <aislqmovic@gmail.com>
2020-06-10 18:09:15 +0200

    Added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>Made by Amel Islamovic</p>
+      <a
+        href="https://github.com/sk0le"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          src="https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/github-512.png"
+          alt=""
+          className="footer-btn"
+        />
+      </a>
+    </footer>
+  );
+}
